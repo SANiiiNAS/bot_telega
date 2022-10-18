006754f8722f0011ebe4b221cb295e57969d813e
SANiiiNAS <114163269+SANiiiNAS@users.noreply.github.com>
2022-10-18 17:56:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const telegramBot = require("node-telegram-bot-api")
 TOKEN = '5655149375:AAH0XldbeSCG__nDnynFVS9dlmhIAoO-cbI'
 const bot = new telegramBot(TOKEN, { polling: true })
 
-bot.sendMessage(msg.chat.id, `hello from Heroku , bot say :'hi,${msg.from.first_name}'`)
+bot.on('message', msg => {
+	bot.sendMessage(msg.chat.id, `hello from Heroku , bot say :'hi,${msg.from.first_name}'`)
+
+})
