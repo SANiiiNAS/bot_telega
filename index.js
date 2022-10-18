215814fa7b5644695c9c96807e04e9d14838b791
SANiiiNAS <114163269+SANiiiNAS@users.noreply.github.com>
2022-10-18 18:16:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ TOKEN = '5655149375:AAH0XldbeSCG__nDnynFVS9dlmhIAoO-cbI'
 const bot = new telegramBot(TOKEN, { polling: true })
 
 bot.on('message', msg => {
-	bot.sendMessage(msg.chat.id, `hello from Heroku , bot say :'hi,${msg.from.first_name}'`)
+	const text = msg.text;
+	const chatId = msg.chat.id;
+
+	if (text === '/start') {
+		await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/9a5/3d6/9a53d66b-53c8-3ccb-a3dd-75fa64c18322/20.webp')
+		return bot.sendMessage(chatId, `Добро опожаловать в Телеграм бот Сергея Васильева `)
+
+	}
+	if (text === '/info') {
+		return bot.sendMessage(chatId, `Привет ${msg.from.first_name} ${msg.from.last_name}`)
+	}
+
+
+	// bot.sendMessage(msg.chat.id, `hello from Heroku , bot say :'hi,${msg.from.first_name}'`)
 
 })
+
+
+
+
